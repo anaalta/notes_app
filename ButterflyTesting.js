f112d21efd145cc0bc430cead0d5e08adfd6abda
Sam <sblausten@Sams-Mac.local>
2017-05-23 13:16:58 +0100

    Refactored code and made syntax like jasmine

--- ButterflyTesting.js
+++ ButterflyTesting.js
@@ -1,39 +1,41 @@
-isTruthy = function(test_name, statement) {
-  if (statement) {
-    testPassing(test_name);
-  } else {
-    testFailing(test_name);
+
+assert = module.exports = {
+  isTruthy: function(statement) {
+      if (statement) {
+          result('PASSED');
+      } else {
+          result('FAILED');
+      }
+  },
+
+  isEqual: function(statement1, statement2) {
+      if (statement1 === statement2) {
+          result('PASSED');
+      } else {
+          result('FAILED');
+      }
+  },
+
+  include: function(includedObject, inclusiveObject) {
+      if (inclusiveObject.includes(includedObject)) {
+          result('PASSED');
+      } else {
+          result('FAILED');
+      }
   }
 };
 
-var testPassing = function(test_name) {
-  console.log(' '.repeat(indentLevel) + 'PASSING TEST: "' + test_name + '"');
-};
+var result = function(result) {
+      console.log(' '.repeat(indentLevel) + result);
+  };
 
-var testFailing = function(test_name) {
-  console.log(' '.repeat(indentLevel) + 'FAILING TEST: "' + test_name + '"');
-};
-
-isEqual = function(test_name, statement1, statement2) {
-  if (statement1 === statement2) {
-    testPassing(test_name);
-  } else {
-    testFailing(test_name);
-  }
-};
-
-include = function(test_name, includedObject, inclusiveObject) {
-  if (inclusiveObject.includes(includedObject)) {
-    testPassing(test_name);
-  } else {
-    testFailing(test_name);
-  }
-};
 var indentLevel = 0;
 
-describe = function(description, test){
-  console.log(' '.repeat(indentLevel) + description);
-  indentLevel += 2;
-  test();
-  indentLevel -= 2;
-};
+describe = function(description, test) {
+      console.log(' '.repeat(indentLevel) + description);
+      indentLevel += 2;
+      test();
+      indentLevel -= 2;
+}
+
+it = describe;
--- NoteSpec.js
+++ NoteSpec.js
@@ -1,28 +1,30 @@
-require ('./ButterflyTesting.js');
+require('./ButterflyTesting.js');
 require('./Note.js');
 var Note = require('./Note.js');
 
 var note = new Note('My first note is more than 20 characters');
+var note2 = new Note('A short note');
 
-describe('note.text', function(){
-  describe('nothing has really happened', function(){
-  isEqual('stores parameter passed to new Note',
-          note.text,
-          'My first note is more than 20 characters');
-  });
+describe('note.text', function() {
+    it('stores parameter passed to new Note', function() {
+        assert.isEqual(note.text, 'My first note is more than 20 characters');
+    });
 });
 
 describe('note.summary', function() {
-isEqual('returns first 20 chars of a note',
-         note.summary(),
-        'My first note is mor...');
-
+    it('returns first 20 chars of a note', function() {
+        assert.isEqual(note.summary(), 'My first note is mor...');
+    });
+    it('includes no ellipses when note.text is <= 20 characters', function() {
+        assert.isEqual(note2.summary(), 'A short note');
+    });
 });
-//var note2 = new Note('A short note');
-//isEqual('includes no ellipses when note.text is <= 20 characters',
-//        note2.summary(),
-//       'A short note');
-//
-//describe('Note.all');
-//include('note 2 is in Note.all', note2, Note.all);
-//include('note is in Note.all', note, Note.all);
+
+describe('note.all', function() {
+    it('note 2 is in Note.all', function() {
+        assert.include(note2, Note.all);
+    });
+    it('note is in Note.all', function() {
+        assert.include(note, Note.all);
+    });
+});
