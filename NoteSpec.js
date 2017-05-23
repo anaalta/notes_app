1acf79c457d4337b27f2b4221f5ecc71476b9d71
Alex Chalk <alexchalk17@gmail.com>
2017-05-23 09:07:39 -0400

    Add alt Butterfly Framework based on prior model

--- NoteSpec.js
+++ NoteSpec.js
@@ -7,6 +7,7 @@ describe('note.text');
 isEqual('stores parameter passed to new Note',
          note.text,
         'My first note is more than 20 characters');
+endDescribe();
 
 describe('note.summary');
 isEqual('returns first 20 chars of a note',
@@ -17,7 +18,10 @@ var note2 = new Note('A short note');
 isEqual('includes no ellipses when note.text is <= 20 characters',
         note2.summary(),
        'A short note');
+endDescribe();
 
 describe('Note.all');
 include('note 2 is in Note.all', note2, Note.all);
 include('note is in Note.all', note, Note.all);
+endDescribe();
+
